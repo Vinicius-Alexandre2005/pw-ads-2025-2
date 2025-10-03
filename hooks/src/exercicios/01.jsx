833ffea39325956864114ca3b50e8b5092409ded
VINICIUS ALEXANDRE MENDES SILVA <1090482411030@fatec-srv.local>
2025-10-03 09:08:04 -0300

    (03/10) React hooks: exercício 01

--- hooks/src/exercicios/01.jsx
+++ hooks/src/exercicios/01.jsx
@@ -2,25 +2,27 @@ import * as React from 'react'
 
 function Greeting({initialName}) {
   // 💣 exclua esta declaração de variável e troque por uma chamada a React.useState
-  const name = ''
+  // const name = ''
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 atualize o nome aqui baseado em event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Nome: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name}/>
       </form>
       {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
     </div>
   )
 }
 
-function App() {
+function Exercicio01() {
   return Greeting({initialName: "Turíbio"})
 }
 
-export default App
+export default Exercicio01
